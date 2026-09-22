b63776da34edc3a5081074682ab657aeb91943c5
Sabrina Santos <ssabrinasantosr@gmail.com>
2026-09-21 21:52:10 -0300

    Removendo pastas que nao fazem parte do trabalho

--- desafio c4/atv.py
+++
@@ -1,6 +0,0 @@
-# Exercício 1 - Números ímpares de 20 até 0 em ordem decrescente
-numero = 19  # Primeiro número ímpar menor que 20
-
-while numero > 0:
-    print(numero)
-    numero -= 2  # Decrementa de 2 em 2 para pegar o próximo ímpar
